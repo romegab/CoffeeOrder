0d5661d0bd462d7607a30295a97bae05bc81aa68
romegab <idstoilov@gmial.com>
2023-05-28 15:25:37 +0300

    Done with decoding structs

--- CoffeeOrder/Controllers/OrdersTableViewController.swift
+++ CoffeeOrder/Controllers/OrdersTableViewController.swift
@@ -9,4 +9,27 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        populateOrders()
+    }
+    
+    private func populateOrders() {
+        guard let coffeeOrdersURL = URL(string: "https://warp-wiry-rugby.glitch.me/orders") else {
+             fatalError("URL for coffee orders is incorrect")
+         }
+        
+        let resource = Resource<[Order]>(url: coffeeOrdersURL )
+        
+        WebService().load(resource: resource) { result in
+            switch result {
+            case .success(let orders):
+                print(orders)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
 }
